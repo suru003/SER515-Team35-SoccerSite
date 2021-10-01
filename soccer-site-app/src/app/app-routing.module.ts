8e3ca04c0d514386a84fb5db7086a324a65e00fd
Jennifer Sarfo <jbsarfo@asu.edu>
2021-10-01 01:14:52 +0000

    Add frontend for adding coach, add routes

--- soccer-site-app/src/app/app-routing.module.ts
+++ soccer-site-app/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 import { CoachComponent } from './coach/coach.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { Admin2Component } from './admin2/admin2.component';
+import { AddCoachComponent } from './admin2/addCoach/addCoach.component';
 
 const routes: Routes = [
   {path:'app-coach', component: CoachComponent},
   {path:'app-homepage', component: HomepageComponent},
   {path:'admin-home', component: AdminHomeComponent},
+  {path:'admin2-page', component: Admin2Component},
+  {path:'addcoach-page', component: AddCoachComponent},
   {path:'', component:HomepageComponent}
 ];
 
