211939fc27e823aee571992ac5216285d43e5cf4
shantanuojha <shantanu.ojha49@gmail.com>
2021-10-31 16:02:41 -0700

    commiting FAQ component page creation

---
+++ soccer-site-app/src/app/faqpage/faqpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faqpage',
+  templateUrl: './faqpage.component.html',
+  styleUrls: ['./faqpage.component.css']
+})
+export class FAQPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
