b2be92fbd3dad5003782252df4ce0077a5bb25fe
Jennifer Sarfo <jbsarfo@asu.edu>
2021-11-04 19:34:25 +0000

    Correcting navbar

---
+++ taiga_assignment/src/test/java/com/taiga/assignment/TaigaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiga.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaigaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
