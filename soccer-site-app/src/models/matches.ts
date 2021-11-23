8cbe981c0292831fecaf835bb03c583da378a43e
Sahithi <brvsahithi@gmail.com>
2021-11-23 11:04:59 -0700

    Modified viewing matches

--- soccer-site-app/src/models/matches.ts
+++ soccer-site-app/src/models/matches.ts
@@ -1,4 +1,13 @@
 export class Matches{
-    teamNames!: String;
+    
     date!: String;
+    category !: String;
+    home_team !: String;
+    team_name2 !: String;
+    time !: String;
+    team_name1 !: String;
+    
+    
+    
+    
 }
